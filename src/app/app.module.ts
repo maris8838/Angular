4b0c7311c06f89127b0e5186aeba3fbe222c44e3
maris2366257 <maris2366257@gmail.com>
2019-06-25 19:06:25 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { Content1Component } from './content1/content1.component';
+import { Content2Component } from './content2/content2.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PostComponent } from './post/post.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    BannerComponent,
+    Content1Component,
+    Content2Component,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    PostComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
